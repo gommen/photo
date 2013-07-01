e2854429fbafce1c7b87d7f0828429fe9da5c2cb
gommen <michael.k.thomsen@gmail.com>
2013-07-01 22:44:24 +0200

    fixed

--- tools/raspistill.js
+++ tools/raspistill.js
@@ -1,13 +1,31 @@
 var spawn = require('child_process').spawn;
+var winston = require("winston");
 
-var rapsitill_cmd = 'ls';
-var raspistill_param = ['-lart'];
-function raspistill(params, callback) {
+var rapsitill_cmd = '/Users/michael/bin/raspistill';
+var raspistill_param = [];
+function raspistill(params, data_callback, end_callback) {
   var cmd = spawn(rapsitill_cmd, raspistill_param);
-  cmd.stdout.on('data', callback);
+  cmd.stdout.on('data', data_callback);
+  cmd.stdout.on('end', end_callback);
+  cmd.on('end', end_callback);
 }
 
 
-raspistill({}, function (data) {
-  console.log('data', data.toString());
-});
+function handle_photo(req, res) {
+  //Extract the parameters from the request
+  //Set the header 
+  res.writeHead(200, {'Content-Type': 'image/jpg'});
+  //call raspistill
+  raspistill({}, function (data) {
+    res.write(data);
+    winston.info('got a chunk of data');
+
+  },
+    function (data) {
+      res.end();
+      winston.info('data stream closed');
+    }
+    );
+}
+
+exports.handle_photo = handle_photo;
